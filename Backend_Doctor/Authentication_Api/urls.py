9f70c4d0cb4e37373cd229cb3ad5f8dc254adde2
saugat1070 <saugatgiri1070@gmail.com>
2025-04-19 15:44:26 +0545

    handling url configuration of profile

--- Backend_Doctor/Authentication_Api/urls.py
+++ Backend_Doctor/Authentication_Api/urls.py
@@ -1,5 +1,5 @@
 
-from django.urls import path,include
+from django.urls import path
 from Authentication_Api import views
 from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
