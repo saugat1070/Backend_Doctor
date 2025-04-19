d8c86cce4f47fb569f8e3261af5124b730bed654
saugat1070 <saugatgiri1070@gmail.com>
2025-04-19 18:05:47 +0545

    include profile update in urlspatterns

--- Backend_Doctor/Authentication_Api/urls.py
+++ Backend_Doctor/Authentication_Api/urls.py
@@ -1,6 +1,8 @@
 
 from django.urls import path
 from Authentication_Api import views
+# from Authentication_Api.views import Profile_update
+
 from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 
@@ -9,6 +11,7 @@ router.register(r'login', views.Login, basename='loign')
 # router.register(r'profile',views.Profile, basename='profile')
 
 urlpatterns = [
-    path('profile/',views.Profile.as_view({'get':'retrieve'}),name='profile')
+    path('profile/',views.Profile.as_view({'get':'retrieve'}),name='profile'),
+    path('profile_update/',views.Profile_update.as_view({'put':'update'}))
 ]
 urlpatterns += router.urls
