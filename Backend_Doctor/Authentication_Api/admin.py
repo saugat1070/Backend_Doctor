e432015ce923bde29b7d78fe6c050488e458fadc
saugat1070 <saugatgiri1070@gmail.com>
2025-07-22 14:21:21 +0545

    delete all files

--- Backend_Doctor/Authentication_Api/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from Authentication_Api.models import UserRegistration
-# Register your models here.
-
-admin.site.register(UserRegistration)
--- Backend_Doctor/api_project/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.urls import path,include
-from api_project import views
-
-urlpatterns = [
-   path('',views.greet,name='greet'),
-   
-]
